7129b831461983389e10bba55313b028823e1b50
Anja <anjavangestel92@gmail.com>
2021-07-08 13:17:26 +0200

    installation-verificiation script (initial version)

---
+++ installation-verification.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#######################################
+# Bash script to verify that the installation script was successful. 
+# Run with 'bash installation-verification.sh'
+# Written by Frank and Anja from IKNL
+#######################################
+
+REPORT='~/installation-report.txt'
+touch $REPORT
+
+## Installed packages ##
+echo '## Check installed packages ##' >> $REPORT
+pip list >> $REPORT
+docker info >> $REPORT
+
+## Conda ##
+echo '## Check that conda is functional ##' >> $REPORT
+conda list >> $REPORT
+
+## vantage6 ##
+echo '## Check that vantage6 is functional ##' >> $REPORT
+vnode list >> $REPORT
+vnode files --name starter_head_and_neck --environment application >> $REPORT
+
+## Docker ##
+echo '## Check that docker is running ##' >> $REPORT
+if [ "$(systemctl is-active docker)" = "active" ] 
+then echo '[OK] docker is running' >> $REPORT
+else echo '[ERR] docker is not  running...' >> $REPORT
+fi
+
+echo '## Run docker hello-world (without sudo) ##' >> $REPORT
+docker run hello-world >> $REPORT
