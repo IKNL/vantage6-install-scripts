0cad8c0633c5b3186712db328127fe2792f92e95
Frank Martin <f.martin@iknl.nl>
2021-09-21 09:43:55 +0200

    Update configure-node.sh
    Creation of task-directory

--- configure-node.sh
+++ configure-node.sh
@@ -1,9 +1,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR="$HOME/.config/vantage6/node"
+TASK_DIR="$HOME/.local/share/vantage6/node/starter_head_and_neck"
 DATA_FILE="$HOME/empty-data.csv"
 
-echo "> Creating directory to store configuration"
+echo "> Creating directory to store configuration and task-data"
 mkdir -p $OUTPUT_DIR
+mkdir -p $TASK_DIR
 
 echo "> Creating empty data file"
 touch $DATA_FILE
@@ -14,4 +16,5 @@ read API_KEY
 echo "> Parsing & saving configuration file"
 export API_KEY=$API_KEY
 export DATA_FILE=$DATA_FILE
+export TASK_DIR=$TASK_DIR
 envsubst < $SCRIPT_DIR/config.tpl > $OUTPUT_DIR/starter_head_and_neck.yaml 
