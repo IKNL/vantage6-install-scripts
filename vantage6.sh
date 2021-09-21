0c66b1e8794f2644e2f6e85c31c7a47d4df30a8f
Frank Martin <f.martin@iknl.nl>
2021-09-21 09:31:31 +0200

    Update vantage6.sh

--- vantage6.sh
+++ vantage6.sh
@@ -15,7 +15,7 @@ REPORT=vantage6-installation-report.txt
 touch $REPORT
 
 ## Update packages and Upgrade system
-echo '###Update/upgrade system' > $REPORT
+echo '###Update/upgrade system' >> $REPORT
 apt-get update -y >> $REPORT
 apt-get upgrade -y >> $REPORT
 apt-get install systemd -y >> $REPORT
@@ -52,7 +52,7 @@ echo '###Installing Docker..'
 chmod +x $SCRIPT_DIR/get-docker.sh
 $SCRIPT_DIR/get-docker.sh >> $REPORT
 # Manage Docker as a non-root user
-echo '##Manage Docker as a non-root user..'
+echo '###Manage Docker as a non-root user..'
 # Create the docker group
 groupadd docker
 # Add your user and root to the docker group
