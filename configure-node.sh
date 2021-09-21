dc7aa5dbc9fc173f8fe63a36183db37310a20acd
Frank Martin <f.martin@iknl.nl>
2021-09-21 09:36:51 +0200

    Update configure-node.sh
    Added creation of empty data-file

--- configure-node.sh
+++ configure-node.sh
@@ -1,9 +1,17 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR="$HOME/.config/vantage6/node"
+DATA_FILE="$HOME/empty-data.csv"
+
+echo "> Creating directory to store configuration"
 mkdir -p $OUTPUT_DIR
 
-echo "Enter your API KEY:"
+echo "> Creating empty data file"
+touch $DATA_FILE
+
+echo "> Please enter your API KEY:"
 read API_KEY
 
+echo "> Parsing & saving configuration file"
 export API_KEY=$API_KEY
+export DATA_FILE=$DATA_FILE
 envsubst < $SCRIPT_DIR/config.tpl > $OUTPUT_DIR/starter_head_and_neck.yaml 
