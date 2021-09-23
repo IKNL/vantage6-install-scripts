fb2a659c66448f4882a251c840805693695f2221
matteogabetta <matteo.gabetta@gmail.com>
2021-09-23 10:34:45 +0200

    typo

--- biomeris/install.sh
+++ biomeris/install.sh
@@ -22,7 +22,7 @@ sudo apt-get -y install git-core &>> $REPORT
 sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024 &>> $REPORT
 sudo /sbin/mkswap /var/swap.1 &>> $REPORT
 sudo /sbin/swapon /var/swap.1 &>> $REPORT
-sudo sh -c 'echo "/var/swap.1 swap swap defaults 0 0 " &>> /etc/fstab'
+sudo sh -c 'echo "/var/swap.1 swap swap defaults 0 0 " >> /etc/fstab'
 echo "...R installed."
 
 
