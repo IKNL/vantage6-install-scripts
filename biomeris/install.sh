45de2abc119ca3a74214b2c651e30a53cfcf8e4d
matteogabetta <matteo.gabetta@gmail.com>
2021-09-23 10:15:04 +0200

    clean

--- biomeris/install.sh
+++ biomeris/install.sh
@@ -18,33 +18,9 @@ sudo /sbin/mkswap /var/swap.1 >> $REPORT
 sudo /sbin/swapon /var/swap.1 >> $REPORT
 sudo sh -c 'echo "/var/swap.1 swap swap defaults 0 0 " >> /etc/fstab'
 
+#Devtools
 sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/')\"" >> $REPORT
-# sudo su - -c "R -e \"install.packages('Rcpp', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('RcppEigen', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('ggplot2', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('Cairo', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('evaluate', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('formatR', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('highr', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('markdown', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('yaml', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('htmltools', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('knitr', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('rmarkdown', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"install.packages('RColorBrewer', repos='http://cran.rstudio.com/')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('BiocParallel')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('Biobase')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('EBSeq')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('monocle')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('sincell')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('scde')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('scran')\""
-# sudo su - -c "R -e \"source('http://bioconductor.org/biocLite.R'); biocLite('scater')\""
-# sudo su - -c "R -e \"devtools::install_github('kdkorthauer/scDD')\""
-# 
-# wget https://download2.rstudio.org/rstudio-server-0.99.903-amd64.deb
-# 
-# sudo gdebi rstudio-server-0.99.903-amd64.deb
+
 
 #Packages needed by DQ scripts
 sudo su - -c "R -e \"install.packages('REDCapR', repos='http://cran.rstudio.com/')\"" >> $REPORT
@@ -79,9 +55,5 @@ echo "" >> /etc/crontab
 echo "" >> /etc/crontab
 echo "* * * * * root sh /opt/redcap_dq/engine/s3_get_R_run.sh" >> /etc/crontab
 service cron restart >> $REPORT
-
-#TODO (maybe)
-#-store an ENV variable which represents the site ID? (this may be used if we eant to (re)execute am R script only for a group of sites)
-#-mechanism to execute scripts only once (within the "engine" or in the scripts themselves)
 
 bash config.sh
