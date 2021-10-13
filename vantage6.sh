de275c4ef028b54986dd8b2a677fe76ac570bb76
Frank <f.martin@iknl.nl>
2021-10-13 10:43:14 +0200

    Print some info to the user during the installation process

--- vantage6.sh
+++ vantage6.sh
@@ -15,12 +15,13 @@ REPORT=vantage6-installation-report.txt
 touch $REPORT
 
 ## Update packages and Upgrade system
+echo '(1/6) ###Update/upgrade system'
 echo '###Update/upgrade system' >> $REPORT
 apt-get update -y >> $REPORT
 apt-get upgrade -y >> $REPORT
 apt-get install systemd -y >> $REPORT
 
-
+echo '(2/6) ###Installing miniconda'
 echo '###Installing miniconda' >> $REPORT
 # installer script downloaded from:
 # curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ~/miniconda.sh
@@ -33,7 +34,7 @@ eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 # initialize conda
 conda init >> $REPORT
 
-
+echo '(3/6) ###Creating a virtual environment'
 echo '###Creating a virtual environment' >> $REPORT
 # create python virtual environment with python 3.7!
 conda create -n $VENV python=3.7 -y >> $REPORT
@@ -42,17 +43,20 @@ conda activate $VENV
 
 
 ## vantage6 ## >> $REPORT
-echo '###Installing vantage6 $VANTAGE6_VERSION ..'
+echo '(4/6) ###Installing vantage6 $VANTAGE6_VERSION ..'
+echo '###Installing vantage6 $VANTAGE6_VERSION ..' >> $REPORT
 pip install vantage6==$VANTAGE6_VERSION >> $REPORT
 
 
 ## Docker ## >> $REPORT
-echo '###Installing Docker..'
+echo '(5/6) ###Installing Docker..'
+echo '###Installing Docker..' >> $REPORT
 # installer script downloaded from: https://get.docker.com/
 chmod +x $SCRIPT_DIR/get-docker.sh
 $SCRIPT_DIR/get-docker.sh >> $REPORT
 # Manage Docker as a non-root user
-echo '###Manage Docker as a non-root user..'
+echo '(6/6) ###Manage Docker as a non-root user..'
+echo '###Manage Docker as a non-root user..' >> $REPORT
 # Create the docker group
 groupadd docker
 # Add your user and root to the docker group
