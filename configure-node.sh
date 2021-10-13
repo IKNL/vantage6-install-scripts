b3551444ea53a79fc040db3321f11ff6b54f5c6a
Frank <f.martin@iknl.nl>
2021-10-13 10:59:52 +0200

    Added dummy data

--- configure-node.sh
+++ configure-node.sh
@@ -1,14 +1,17 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR="$HOME/.config/vantage6/node"
 TASK_DIR="$HOME/.local/share/vantage6/node/starter_head_and_neck"
-DATA_FILE="$HOME/empty-data.csv"
+DATA_FILE="$HOME/dummy-data.csv"
 
 echo "> Creating directory to store configuration and task-data"
 mkdir -p $OUTPUT_DIR
 mkdir -p $TASK_DIR
 
-echo "> Creating empty data file"
+echo "> Creating dummy data file"
 touch $DATA_FILE
+echo 'Sex,Age' >> $DATA_FILE
+echo 'm,21' >> $DATA_FILE
+echo 'f,19' >> $DATA_FILE
 
 echo "> Please enter your API KEY:"
 read API_KEY
@@ -17,4 +20,4 @@ echo "> Parsing & saving configuration file"
 export API_KEY=$API_KEY
 export DATA_FILE=$DATA_FILE
 export TASK_DIR=$TASK_DIR
-envsubst < $SCRIPT_DIR/config.tpl > $OUTPUT_DIR/starter_head_and_neck.yaml 
+envsubst < $SCRIPT_DIR/config.tpl > $OUTPUT_DIR/starter_head_and_neck.yaml
