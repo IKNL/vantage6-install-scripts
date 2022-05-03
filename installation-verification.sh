26b20d3c9623eb0f8d97a5b0d2d4ff2abf0ea74c
bart <b.vanbeusekom@iknl.nl>
2022-05-03 14:07:11 +0200

    Improved logging in installation verification script, and ensure it is run without sudo

--- installation-verification.sh
+++ installation-verification.sh
@@ -2,8 +2,8 @@
 
 #######################################
 # Bash script to verify that the installation script (vantage6.sh) was successful.
-# Run with 'sudo -E bash installation-verification.sh'
-# Written by Frank and Anja from IKNL
+# Run with 'bash installation-verification.sh'
+# Written by Frank, Anja and Bart from IKNL
 #######################################
 
 REPORT=vantage6-installation-verification-report.txt
@@ -14,24 +14,24 @@ echo '## Check installed packages ##' > $REPORT
 # conda is not available by default in subshells
 source ~/miniconda/etc/profile.d/conda.sh
 conda activate vantage6
-pip list >> $REPORT
-docker info >> $REPORT
+pip list 2>&1 | tee -a $REPORT
+docker info 2>&1 | tee -a $REPORT
 
 ## Conda ##
-echo '## Check that conda is functional ##' >> $REPORT
-conda list >> $REPORT
+echo '## Check that conda is functional ##' 2>&1 | tee -a $REPORT
+conda list 2>&1 | tee -a $REPORT
 
 ## vantage6 ##
-echo '## Check that vantage6 is functional ##' >> $REPORT
-vnode list >> $REPORT
-vnode files --name starter_head_and_neck --environment application >> $REPORT
+echo '## Check that vantage6 is functional ##' | tee -a $REPORT
+vnode list 2>&1 | tee -a $REPORT
+vnode files --name starter_head_and_neck --environment application 2>&1 | tee -a $REPORT
 
 ## Docker ##
-echo '## Check that docker is running ##' >> $REPORT
+echo '## Check that docker is running ##' | tee -a $REPORT
 if [ "$(systemctl is-active docker)" = "active" ]
-then echo '[OK] docker is running' >> $REPORT
-else echo '[ERR] docker is not  running...' >> $REPORT
+then echo '[OK] docker is running' | tee -a $REPORT
+else echo '[ERR] docker is not  running...' | tee -a $REPORT
 fi
 
-echo '## Run docker hello-world (without sudo) ##' >> $REPORT
-docker run hello-world >> $REPORT
+echo '## Run docker hello-world (without sudo) ##' | tee -a $REPORT
+docker run hello-world 2>&1 | tee -a $REPORT
