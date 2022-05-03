798deaa1e63dc0af667e1c7bc8803415fb43a797
bart <b.vanbeusekom@iknl.nl>
2022-05-03 14:13:19 +0200

    Saving last start time when node is started

--- start-node.sh
+++ start-node.sh
@@ -3,8 +3,12 @@
 #######################################
 # Bash script to start the node, which will mean
 # the node will be online and ready for communication with the server.
-# Written by Frank and Anja from IKNL
+# Written by Frank, Anja and Bart from IKNL
 #######################################
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOG_DIR="${SCRIPT_DIR}/log"
+LAST_START="${LOG_DIR}/last_restart.v6"
 
 ## activate the vantage6 venv ##
 # conda is not available by default in subshells
@@ -15,3 +19,6 @@ conda activate vantage6
 vnode start --config starter_head_and_neck.yaml
 
 vnode list
+
+echo "> Saving last start time"
+date +"%Y-%m-%d %T" > $LAST_START
