d4250a4895ffb277a9b36703adaf45f46712b859
bart <b.vanbeusekom@iknl.nl>
2022-05-04 15:18:13 +0200

    Updated node configuration

--- configure-node.sh
+++ configure-node.sh
@@ -1,7 +1,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR="$HOME/.config/vantage6/node"
 TASK_DIR="$HOME/.local/share/vantage6/node/starter_head_and_neck"
-DATA_FILE="$HOME/dummy-data.csv"
+DATA_DIR="$HOME/data"
+DATA_FILE="$DATA_DIR/default.csv"
+
+# create data directory and file if they don't exist
+mkdir -p $DATA_DIR
+touch $DATA_FILE
 
 echo "> Creating directory to store configuration and task-data"
 mkdir -p $OUTPUT_DIR
@@ -9,7 +14,7 @@ mkdir -p $TASK_DIR
 
 echo "> Creating dummy data file"
 touch $DATA_FILE
-echo 'Sex,Age' >> $DATA_FILE
+echo 'Sex,Age' > $DATA_FILE
 echo 'm,21' >> $DATA_FILE
 echo 'f,19' >> $DATA_FILE
 
