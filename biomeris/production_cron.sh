900f77a650a5a8d6941aa8f23ca870dfddd9b5b2
matteogabetta <matteo.gabetta@gmail.com>
2022-09-20 12:33:57 +0200

    gruppo docker + crontab scripts

---
+++ biomeris/production_cron.sh
@@ -0,0 +1,10 @@
+echo "This script reschedules remote script fetching and executing every 30 minutes (e.g. 10:00 - 10:30 - 11.00 - ...)"
+service cron stop
+echo "CRON Service stopped"
+echo "\nPrevious crontab file:\n"
+cat /etc/crontab
+sed -i 's/\* \* \* \* \* vantage_user/30 * * * * vantage_user/' /etc/crontab
+echo "\nNew crontab file:\n"
+cat /etc/crontab
+service cron start
+echo "CRON Service started"
---
+++ biomeris/test_cron.sh
@@ -0,0 +1,10 @@
+echo "This script reschedules remote script fetching and executing every minute (e.g. 10:00 - 10:01 - 10.02 - ...)"
+service cron stop
+echo "CRON Service stopped"
+echo "\nPrevious crontab file:\n"
+cat /etc/crontab
+sed -i 's/30 \* \* \* \* vantage_user/* * * * * vantage_user/' /etc/crontab
+echo "\nNew crontab file:\n"
+cat /etc/crontab
+service cron start
+echo "CRON Service started"
