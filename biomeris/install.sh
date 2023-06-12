cc95613b2b7de5c94e79817438fab389b91d6831
matteogabetta <matteo.gabetta@gmail.com>
2023-06-12 11:28:19 +0200

    BIOMERIS 1.1 + UPDATE from 1.0 (tentative)

--- biomeris/install.sh
+++ biomeris/install.sh
@@ -38,30 +38,30 @@ sudo su - -c "R -e \"install.packages('https://cran.r-project.org/src/contrib/Ar
 
 echo "...still in progress..."
 #Packages needed by DQ scripts
-sudo su - -c "R -e \"remotes::install_version('REDCapR', version = '1.0.0', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('REDCapR', version = '1.0.0', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('plyr', version = '1.8.6', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('plyr', version = '1.8.6', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('dplyr', version = '1.0.8', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('dplyr', version = '1.0.8', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('openxlsx', version = '4.2.4', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('openxlsx', version = '4.2.4', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('properties', version = '0.0.9', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('properties', version = '0.0.9', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('prodlim', version = '2019.11.13', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('prodlim', version = '2019.11.13', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('data.table', version = '1.14.2', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('data.table', version = '1.14.2', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('formattable', version = '0.2.1', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('formattable', version = '0.2.1', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('gtools', version = '3.9.2', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('gtools', version = '3.9.2', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
 #sudo add-apt-repository -y ppa:cran/poppler &>> $REPORT
 #sudo apt-get update &>> $REPORT
 sudo apt-get install -y libpoppler-cpp-dev &>> $REPORT
-sudo su - -c "R -e \"remotes::install_version('rjson', version = '0.2.20', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('rjson', version = '0.2.20', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...still in progress..."
-sudo su - -c "R -e \"remotes::install_version('readtext', version = '0.81', repos = 'https://cran.us.r-project.org')\"" &>> $REPORT
+sudo su - -c "R -e \"remotes::install_version('readtext', version = '0.81', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
 echo "...R packages installed."
 
 echo "STEP 4/7"
@@ -109,7 +109,7 @@ service cron restart &>> $REPORT
 echo "Cronjob ok."
 
 touch /opt/redcap_dq/engine/version
-echo "1.0" >> /opt/redcap_dq/engine/version
+echo "1.1" >> /opt/redcap_dq/engine/version
 
 echo "STEP 7/7"
 echo "Site DQ configuration..."
--- biomeris/update.sh
+++ biomeris/update.sh
@@ -1,1 +1,30 @@
-echo "No update available"
+if grep -q "1.0" /opt/redcap_dq/engine/version; then
+    sudo su - -c "R -e \"remotes::install_version('REDCapR', version = '1.0.0', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('plyr', version = '1.8.6', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('dplyr', version = '1.0.8', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('openxlsx', version = '4.2.4', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('properties', version = '0.0.9', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('prodlim', version = '2019.11.13', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('data.table', version = '1.14.2', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('formattable', version = '0.2.1', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('gtools', version = '3.9.2', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    #sudo add-apt-repository -y ppa:cran/poppler &>> $REPORT
+    #sudo apt-get update &>> $REPORT
+    sudo apt-get install -y libpoppler-cpp-dev &>> $REPORT
+    sudo su - -c "R -e \"remotes::install_version('rjson', version = '0.2.20', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...still in progress..."
+    sudo su - -c "R -e \"remotes::install_version('readtext', version = '0.81', repos = 'https://cloud.r-project.org')\"" &>> $REPORT
+    echo "...R packages installed."
+
+    echo "1.1_updated" >> /opt/redcap_dq/engine/version
+else
+	echo "No update available"
