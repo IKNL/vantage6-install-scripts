0d049f5e349f6cbdae08d37f450128d7fca86b0b
matteogabetta <matteo.gabetta@gmail.com>
2025-02-12 15:11:31 +0100

    typo

--- cron-restart-v6.sh
+++ cron-restart-v6.sh
@@ -3,8 +3,8 @@
 mkdir -p $HOME/v6-restart-log
 echo "" >> /etc/crontab
 echo "" >> /etc/crontab
-echo "0 14 * * * $SUDO_USER bash $HOME/vantage6-install-scripts/stop-node.sh && echo \"\$(date '+\%Y-\%m-\%d \%H:\%M:\%S') - V6 Node stopped\" >> $HOME/v6-restart-log/vantage_node_restart.log" >> /etc/crontab
-echo "1 14 * * * $SUDO_USER bash $HOME/vantage6-install-scripts/start-node.sh && echo \"\$(date '+\%Y-\%m-\%d \%H:\%M:\%S') - V6 Node started\" >> $HOME/v6-restart-log/vantage_node_restart.log" >> /etc/crontab
+echo "0 1 * * * $SUDO_USER bash $HOME/vantage6-install-scripts/stop-node.sh && echo \"\$(date '+\%Y-\%m-\%d \%H:\%M:\%S') - V6 Node stopped\" >> $HOME/v6-restart-log/vantage_node_restart.log" >> /etc/crontab
+echo "1 1 * * * $SUDO_USER bash $HOME/vantage6-install-scripts/start-node.sh && echo \"\$(date '+\%Y-\%m-\%d \%H:\%M:\%S') - V6 Node started\" >> $HOME/v6-restart-log/vantage_node_restart.log" >> /etc/crontab
 service cron restart
 
 echo "send this to matteo.gabetta@biomeris.it:"
