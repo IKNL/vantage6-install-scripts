b736edbf45fa8915c9c065fca3d239d33284c519
matteogabetta <matteo.gabetta@gmail.com>
2025-02-21 10:05:12 +0100

    update vantage6 install script for cron restart

--- vantage6.sh
+++ vantage6.sh
@@ -97,11 +97,13 @@ fi
 
 #STOP & START evry night
 mkdir -p $HOME/v6-restart-log
+chown -R $SUDO_USER: $HOME/v6-restart-log
 echo "" >> /etc/crontab
 echo "" >> /etc/crontab
 echo "0 1 * * * $SUDO_USER bash $HOME/vantage6-install-scripts/stop-node.sh && echo \"\$(date '+\%Y-\%m-\%d \%H:\%M:\%S') - V6 Node stopped\" >> $HOME/v6-restart-log/vantage_node_restart.log" >> /etc/crontab
 echo "5 1 * * * $SUDO_USER bash $HOME/vantage6-install-scripts/start-node.sh && echo \"\$(date '+\%Y-\%m-\%d \%H:\%M:\%S') - V6 Node started\" >> $HOME/v6-restart-log/vantage_node_restart.log" >> /etc/crontab
 service cron restart
+cat /etc/crontab | tee -a $REPORT
 
 ## Reboot ##
 echo '###Rebooting system in 10 seconds.'
